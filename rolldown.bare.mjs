4488877ae64a2b013757fb97ad45b1edb4d96172
“Isaac <isaac@isaacmason.com>
2026-06-21 17:20:54 +1000

    fix: cleanup rolldown configs

--- rolldown.bare.mjs
+++
@@ -1,6 +0,0 @@
-export default [{
-    input: './src/index.ts',
-    external: [],
-    output: [{ file: 'dist-test/index.js', format: 'es', sourcemap: false, exports: 'named' }],
-    plugins: [],
-}];
--- rolldown.compilecat-test.mjs
+++
@@ -1,7 +0,0 @@
-import compilecat from 'compilecat/rolldown';
-export default [{
-    input: './src/index.ts',
-    external: [],
-    output: [{ file: 'dist-test/index.js', format: 'es', sourcemap: false, exports: 'named' }],
-    plugins: [compilecat({ debug: false })],
-}];
